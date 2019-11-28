5b724436a0b8c37d20aa789a12a3af516197d8d2
othman4893 <othman4893@gmail.com>
2019-11-28 23:25:18 +0100

    Complete all sprite automation

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,35 @@ var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var replace = require('gulp-replace');
+var rename = require('gulp-rename')
+var del = require('del')
 
 var mixins = require('postcss-mixins');
 var cssImporter = require('postcss-import');
-const { src, dest, watch, series } = require('gulp');
+
+
+
+var svgSprite = require('gulp-svg-sprite');
+var config = {
+  mode: {
+    css: {
+      sprite: 'sprite.svg',
+      render: {
+        css: {
+          template: './app/gulp/templates/sprite.css'
+        }
+      }
+    }
+  }
+};
+
+
+const {
+  src,
+  dest,
+  watch,
+  series
+} = require('gulp');
 
 browserSync = require('browser-sync').create();
 
@@ -18,12 +43,35 @@ const files = {
   htmlPathAll: 'app/**/*.html'
 };
 
+/*
 //html
 var cbString = new Date().getTime();
+
 function cashBurtTask() {
   return src(files.htmlPath)
     .pipe(replace(/cd=\d+/g, 'cb=' + cbString));
 }
+*/
+
+// svg sprite
+function svgSpriteTask() {
+  del(['./app/temp/sprite', './app/assets/images/sprites']);
+  return src('./app/assets/images/icons/**/*.svg')
+    .pipe(svgSprite(config))
+    .pipe(dest('./app/temp/sprite/'));
+}
+
+function copySpriteCss() {
+  return src('./app/temp/sprite/css/*.css')
+    .pipe(rename('_sprite.css'))
+    .pipe(dest('./app/assets/styles/modules'))
+}
+
+function copySpriteGraphic() {
+  return src('./app/temp/sprite/css/**/*.svg')
+    .pipe(dest('./app/assets/images/sprites'))
+}
+
 
 function styles() {
   return src('./app/assets/styles/styles.css')
@@ -39,6 +87,7 @@ function browserSyncTask() {
     }
   });
 }
+
 function reloadTask() {
   browserSync.reload();
   return src(files.htmlPath);
@@ -48,8 +97,9 @@ function watchMyFiles() {
   browserSyncTask();
   watch(
     [files.cssPath, files.htmlPath, files.cssPathAll, files.htmlPathAll],
-    series(cashBurtTask, styles, reloadTask)
+    series(styles, reloadTask)
   );
+  del(['./app/temp/sprite']);
 }
 
-exports.default = series(cashBurtTask, styles, watchMyFiles);
+exports.default = series(styles, svgSpriteTask, copySpriteCss, copySpriteGraphic, watchMyFiles);
