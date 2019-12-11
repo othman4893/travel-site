f279fb4fba4619312eca0272725b2b4303d01919
othman4893 <othman4893@gmail.com>
2019-12-11 22:57:04 +0100

    Completed sticky header functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -14,8 +14,10 @@ export default Person;
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import $ from 'jquery'
+import StickyHeader from './modules/StickyHeader'
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($('.feature-item'), "85%");
 new RevealOnScroll($('.testimonial'), "60%");
 
+var stickyHeader = new StickyHeader();
